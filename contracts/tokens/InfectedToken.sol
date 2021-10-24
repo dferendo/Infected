42c14a0611022a284b991f7e49be1343b7cf6a10
ferendo <dylanfrendo@gmail.com>
2021-10-24 15:13:09 +0200

    Added infected token

---
+++ contracts/tokens/InfectedToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract InfectedToken is ERC1155, Ownable {
+    uint256 public constant HOLY = 0;
+    uint256 public constant INFECTED = 1;
+
+    constructor(address[] _holy, address[] _infected) public ERC1155("INFECTED GAME") {
+        _mint(address(this), HOLY, _holy.length, "");
+        _mint(address(this), INFECTED, _infected.length, "");
+    }
+
+}
